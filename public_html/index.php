9b869901509058815c75eabbf6327db764f21515
Udlei Nati <udlei@nati.biz>
2014-05-21 00:56:41 -0300

    Versao com analytics

--- public_html/index.php
+++ public_html/index.php
@@ -75,6 +75,16 @@ $uptime = shell_exec("cut -d. -f1 /proc/uptime");
 
     </script>
     
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-51158860-1', 'redirect.center');
+  ga('send', 'pageview');
+</script>
+
     <style>
 
 body{
