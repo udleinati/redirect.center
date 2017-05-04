1f2adba251394242222bead43b0407e439124b9c
Udlei Nati <udlei@protonmail.ch>
2017-05-04 23:23:31 +0200

    Add vhost base

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  'port': process.env.PORT || 3000,
+  'fqdn': process.env.FQDN || 'localhost'
+}
+
+export default config
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello World')
+import express from 'express'
+import vhost from 'vhost'
+import config from './config'
+
+const app = express()
+
+app.use(vhost(config.fqdn, (req, res) => {
+  res.status(200).send('Public page')
+}))
+
+app.use(vhost(`*.${config.fqdn}`, (req, res) => {
+  res.status(200).send('Redirect page')
+}))
+
+app.listen(config.port, () => {
+  console.log(`Example app listening on port ${config.port}!`)
+})
