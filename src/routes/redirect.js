d98330daad5a54f733c5addf1630aa7d6dd9f603
Udlei Nati <udlei@protonmail.ch>
2017-05-05 13:00:06 +0200

    Add error page

--- src/routes/redirect.js
+++ src/routes/redirect.js
@@ -2,6 +2,7 @@ import Router from 'express'
 import dns from 'dns'
 import parseDomain from 'parse-domain'
 
+import config from '../config'
 import RedirectService from '../services/redirect.service'
 import LoggerHandler from '../handlers/logger.handler'
 
@@ -15,21 +16,43 @@ export default () => {
     const path = `Host ${host}`
     logger.info(path)
 
-    host = 'nati.biz'
-    if (!parseDomain(host).subdomain) {
+    if (parseDomain(host) && !parseDomain(host).subdomain) {
       logger.info(`${path} A:ROOT DOMAIN`)
       targetHost = `redirect.${host}`
     }
 
     dns.resolve(targetHost, 'CNAME', (err, records) => {
       logger.info(`Host ${targetHost} resolve CNAME`)
-      if (err) console.error(err)
+
+      if (!err && records.length > 1) {
+        err = {
+          code: 'MORETHANONE',
+          message: `More than one record on the host ${targetHost}. Found: ${records.join(', ')}`
+        }
+      }
+
+      if (!err && !parseDomain(records[0])) {
+        err = {
+          code: 'NOTADOMAIN',
+          message: `The record on the host ${targetHost} is not valid. Found: ${records[0]}`
+        }
+      }
+
+      if (err) {
+        const context = {
+          config: config,
+          err: err,
+          targetHost: targetHost
+        }
+
+        return res.status(500).render('error.ejs', context)
+      }
 
       const redirectService = new RedirectService(req, res)
       redirectService.perform(records[0]).then((returns) => {
         const url = `${returns.protocol}://${returns.hostname}/${returns.path}`
         logger.info(`${path} redirect ${returns.statusCode} to ${url}`)
-        return res.status(returns.statusCode).send(url)
+        return res.redirect(returns.statusCode, url)
       })
     })
   })
