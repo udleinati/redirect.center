f7f0194c5d632b54200e0333be1fdef14f3372d5
Udlei Nati <udlei@protonmail.ch>
2017-05-05 21:28:58 +0200

    Fix URI error

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,21 @@ import redirect from './redirect'
 export default () => {
   let router = Router()
 
+  // http://localhost.com:3000/%c0%ae%c0%ae
+
+  router.use((req, res, next) => {
+    let err = null
+    try {
+      decodeURIComponent(req.path)
+    } catch (e) {
+      err = e
+    }
+    if (err) {
+      return res.status(500).send(err.message)
+    }
+    next()
+  })
+
   router.use(domain)
   router.use(redirect())
 
