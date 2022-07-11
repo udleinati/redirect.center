3ffe2f30fd2ebfbd7501e480f800214b2d3b30e0
Anzulo1984 <59749264+Anzulo1984@users.noreply.github.com>
2022-07-11 22:20:05 +0200

    Update app.config.ts

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,6 +1,7 @@
 export const appConfig = (): AppConfig => ({
   fqdn: process.env.FQDN || 'localhost',
   entryIp: process.env.ENTRY_IP || '127.0.0.1',
+  ip: process.env.IP || '0.0.0.0',
   port: Number(process.env.PORT) || 3000,
   environment: process.env.ENVIRONMENT || 'dev1',
   nodeEnv: process.env.NODE_ENV || 'development',
@@ -10,6 +11,7 @@ export const appConfig = (): AppConfig => ({
 
 interface AppConfig {
   fqdn: string;
+  ip: string;
   port: number;
   environment: string;
   nodeEnv: string;
